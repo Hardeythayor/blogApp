dbc51a89ab841ef69dd2b9b38ec577cc200468b9
crownbirth Dev 3 <dev3@crownbirth.com>
2020-07-14 18:47:39 +0100

    created the delete button and function

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,7 +4,12 @@
     <a href="/posts" class="btn btn-light mb-5" role="button"><i class="fa fa-arrow-left mr-2"></i>Go back</a>
     <div class="float-right">
     <a href="/posts/{{$post->id}}/edit" class="btn btn-primary mb-5" role="button"><i class="fa fa-edit mr-2"></i></a>
-    <a href="/posts/{{$post->id}}/destroy" class="btn btn-danger mb-5" role="button"><i class="fa fa-trash mr-2"></i></a>  
+
+    {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right ml-2'])!!}
+        {{Form::hidden('_method', 'DELETE')}}
+        <button class="btn btn-danger mb-5" role="button"><i class="fa fa-trash mr-2"></i></button> 
+    {!!Form::close()!!} 
+
     </div>
     <h1>{{ $post->title }}</h1>
     <div>
